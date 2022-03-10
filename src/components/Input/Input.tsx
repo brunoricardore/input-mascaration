98ea35bdf5ff75ca0c6305b972e5a39ceefd6b99
Bruno Ricardo <brunoricardore@gmail.com>
2022-03-10 14:29:41 -0300

    input component created

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-
-export interface InputProps {
-
+import React, { InputHTMLAttributes, useState } from 'react'
+import {formatter} from 'mascaration';
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  mask: string | string[]
+  beforeMask?: (state: string) => string;
 }
 
-const Input: React.FC<InputProps> = (props) => {
+const InputMask: React.FC<InputProps> = (props) => {
+
+  const [state, setState] = useState<string>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const eventValue = event.target.value;
+    let formattedValue = '';
+
+    if (props.beforeMask) {
+      formattedValue = props.beforeMask(eventValue);
+    }
+
+    formattedValue = formatter(props.mask, eventValue);
+    setState(formattedValue);
+  }
+
   return (
-    <input/>
+    <input value={state} onChange={handleChange} />
   )
 }
 
-export default Input;
+export default InputMask;
